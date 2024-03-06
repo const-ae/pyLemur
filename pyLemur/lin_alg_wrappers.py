f1d92de432f709c9e063b753a54e1515a9014bf7
const-ae <artjom31415@googlemail.com>
2024-03-06 10:22:52 +0100

    Implement ridge regression

--- pyLemur/lin_alg_wrappers.py
+++ pyLemur/lin_alg_wrappers.py
@@ -34,3 +34,52 @@ def fit_pca(Y, n, center=True):
         coord_system =  svd.components_
         mean = np.zeros(Y.shape[1])
     return PCA(emb, coord_system, mean)
+
+
+def ridge_regression(Y, X, ridge_penalty = 0, weights = None):
+    """
+    Calculate the ridge regression of a given data matrix Y.
+    Parameters
+    ----------
+    Y : array-like, shape (n_samples, n_features)
+        The input data matrix.
+    X : array-like, shape (n_samples, n_coef)
+        The input data matrix.
+    ridge_penalty : float, default=0
+        The ridge penalty.
+    weights : array-like, shape (n_features,)
+        The weights to apply to each feature.
+    Returns
+    -------
+    ridge: array-like, shape (n_coef, n_features)
+    """
+    n_coef = X.shape[1]
+    n_samples = X.shape[0]
+    n_feat = Y.shape[1]
+    assert Y.shape[0] == n_samples
+    if weights is None:
+        weights = np.ones(n_samples)
+    assert len(weights) == n_samples
+
+    if np.ndim(ridge_penalty) == 0 or len(ridge_penalty) == 1:
+        ridge_penalty = np.eye(n_coef) * ridge_penalty
+    elif np.ndim(ridge_penalty) == 1:
+        assert len(ridge_penalty) == n_coef
+        ridge_penalty = np.diag(ridge_penalty)
+    elif np.ndim(ridge_penalty) == 1:
+        assert ridge_penalty.shape == (n_coef, n_coef)
+        pass
+    else:
+        raise ValueError("ridge_penalty must be a scalar, 1d array, or 2d array")
+        
+    ridge_penalty_sq = np.sqrt(np.sum(weights)) * (ridge_penalty.T @ ridge_penalty)
+    weights_sqrt = np.sqrt(weights)
+    X_ext = np.vstack([multiply_along_axis(X, weights_sqrt, 0), ridge_penalty_sq])
+    Y_ext = np.vstack([multiply_along_axis(Y, weights_sqrt, 0), np.zeros((n_coef, n_feat))])
+
+    ridge = np.linalg.lstsq(X_ext, Y_ext, rcond=None)[0]
+    return ridge
+
+def multiply_along_axis(A, B, axis):
+    # Copied from https://stackoverflow.com/a/71750176/604854
+    return np.swapaxes(np.swapaxes(A, axis, -1) * B, -1, axis)
---
+++ tests/test_lin_alg_utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression, Ridge
+from pyLemur.lin_alg_wrappers import *
+
+def test_fit_pca():
+    # Make example data
+    Y = np.random.randn(30, 400)
+    pca = fit_pca(Y, 3, center = True)
+    assert np.allclose(pca.offset, Y.mean(axis = 0))
+    assert np.allclose(pca.coord_system @ pca.coord_system.T, np.eye(3))
+    assert np.allclose(pca.embedding, (Y - pca.offset) @ pca.coord_system.T)
+
+    pca2 = fit_pca(Y, 3, center = False)
+    assert np.allclose(pca2.offset, np.zeros(Y.shape[1]))
+    assert np.allclose(pca2.coord_system @ pca2.coord_system.T, np.eye(3))
+    assert np.allclose(pca2.embedding, (Y - pca2.offset) @ pca2.coord_system.T)
+
+
+def test_ridge_regression():
+    # Regular least squares
+    Y = np.random.randn(400, 30)
+    X = np.random.randn(400, 3)
+    beta = ridge_regression(Y, X)
+    assert beta.shape == (3, 30)
+    assert np.allclose(beta, np.linalg.inv(X.T @ X) @ X.T @ Y)
+    reg = LinearRegression(fit_intercept=False).fit(X, Y)
+    assert np.allclose(beta, reg.coef_.T)
+
+    # Check with weights
+    weights = np.random.rand(400)
+    beta = ridge_regression(Y, X, weights=weights)
+    reg = LinearRegression(fit_intercept=False).fit(X, Y, sample_weight = weights)
+    assert np.allclose(beta, reg.coef_.T)
+
+    # Check with ridge penalty
+    pen = 0.3
+    beta = ridge_regression(Y, X, ridge_penalty=pen)
+    gamma = np.sqrt(400) * pen**2 * np.eye(3)
+    beta2 = np.linalg.inv(X.T @ X + gamma.T @ gamma) @ X.T @ Y
+    assert np.allclose(beta, beta2)
+
+    reg = Ridge(alpha=pen**4 * 400, fit_intercept=False).fit(X, Y)
+    assert np.allclose(beta, reg.coef_.T)
